8a59c393c878e1c912406582a860858d6636325d
Piotr Osoba <piotrosoba@outlook.com>
2019-08-24 19:55:46 +0200

    portfolio page

---
+++ assets/js/header.js
@@ -0,0 +1,18 @@
+$(() => {
+  const headerH1 = $('#header h1 div')
+  const headerText = 'Piotr Osoba'
+  const headerP = $('#header p')
+  const titleText = 'Front End Developer'
+  let i = 0
+
+  headerH1.html('')
+
+  const headerInterval = setInterval(() => {
+    $('<div>' + headerText[i] + '</div>').appendTo(headerH1).hide().show('puff')
+    i++
+    if (i >= headerText.length) {
+      clearInterval(headerInterval)
+      $('<span>' + titleText + '</span>').appendTo(headerP).hide().delay(300).show('bounce')
+    }
+  }, 300)
+})

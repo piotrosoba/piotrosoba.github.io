87adc6b4875529bd10bc2339a18cc0ede429589a
Piotr Osoba <piotrosoba@outlook.com>
2019-08-25 12:36:45 +0200

    added descriptions to skills

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -6,18 +6,21 @@ skills.hide()
 
 
 function showSkills(evt) {
-  if (win.scrollTop() > skills.position().top + 500) {
+  if (win.scrollTop() > skills.position().top + skills.height() / 2) {
     win.unbind()
 
     skills.each((index, el) => {
-      $(el).delay(index * 500).show('size')
+      $(el).delay(index * 300).show('size')
     })
   }
 }
 
 function showDescription(evt) {
-  const text = $(evt.target).attr('alt')
-  // description.html('<p>' + text + '</p>')
+  $('.skills-icon--active').removeClass('skills-icon--active')
+  const skill = $(evt.target)
+  skill.addClass('skills-icon--active')
+  const text = skill.attr('alt')
+  description.html('<p>' + text + '</p>')
 }
 
 skills.mouseenter(showDescription)

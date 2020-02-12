1a86337807c7ad4064bc58b9baf2a391fb53954b
Piotr Osoba <piotrosoba@outlook.com>
2020-02-12 16:50:05 +0100

    nasz laryngolog

---
+++ nasz-laryngolog/precache-manifest.7049fd3edb17c12b2847f881cf42b94e.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45fa7a1b9636efd7ca0dd92ca0efee0a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9eeeaf3cfc97234b543f",
+    "url": "./static/css/2.c7ccff39.chunk.css"
+  },
+  {
+    "revision": "c620fc5bae6500e3b259",
+    "url": "./static/css/main.6995ee47.chunk.css"
+  },
+  {
+    "revision": "9eeeaf3cfc97234b543f",
+    "url": "./static/js/2.5abc4d7d.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "./static/js/2.5abc4d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c620fc5bae6500e3b259",
+    "url": "./static/js/main.db53a5e9.chunk.js"
+  },
+  {
+    "revision": "8b81dc6398c47f65ff5c",
+    "url": "./static/js/runtime-main.4769a37c.js"
+  },
+  {
+    "revision": "1d9298f311ff47c4bd659b5956df3592",
+    "url": "./static/media/Formularz zgody na udzielenie świadczeń zdrowotnych małoletniemu w obecności opiekuna faktycznego.1d9298f3.pdf"
+  },
+  {
+    "revision": "488c39952e9b630ef7a75e0f256c18c6",
+    "url": "./static/media/Upoważnienie do odbioru dokumentacji medycznej.488c3995.pdf"
+  },
+  {
+    "revision": "da1684d79c53003ee80804f1aa2894fd",
+    "url": "./static/media/Upoważnienie do odbioru wyniku badania.da1684d7.pdf"
+  },
+  {
+    "revision": "dfacf1c4eed659ab3ab7fc24fab208b9",
+    "url": "./static/media/Wniosek o udostępnienie dokumentacji medycznej.dfacf1c4.pdf"
+  },
+  {
+    "revision": "9b52da2a9313639379d4af4942fa8a07",
+    "url": "./static/media/background1.9b52da2a.jpg"
+  },
+  {
+    "revision": "0091779a5a7acc11e9f9a3fd2267af0c",
+    "url": "./static/media/background2.0091779a.jpg"
+  },
+  {
+    "revision": "725a27d2f27f43307c6a7ff66cc0c900",
+    "url": "./static/media/background3.725a27d2.jpg"
+  },
+  {
+    "revision": "be016bc711eb33686f9761f058583d9f",
+    "url": "./static/media/gallery1.be016bc7.jpg"
+  },
+  {
+    "revision": "02ae606baec6e71ae2a961d504ed5c3d",
+    "url": "./static/media/gallery2.02ae606b.jpg"
+  },
+  {
+    "revision": "1e623e60801e57885878345e5e918cf9",
+    "url": "./static/media/gallery3.1e623e60.jpg"
+  },
+  {
+    "revision": "87d8a7d8a791e0c43f3f259493037df3",
+    "url": "./static/media/gallery4.87d8a7d8.jpg"
+  },
+  {
+    "revision": "bc8f3257c3ddbfc3f89e166ce7a65f04",
+    "url": "./static/media/gallery5.bc8f3257.jpg"
+  },
+  {
+    "revision": "4fc19cd240c9f2cf8837c9e0a15796b3",
+    "url": "./static/media/gallery6.4fc19cd2.jpg"
+  },
+  {
+    "revision": "30d416d96ef19a04c4b9c6ba573acc55",
+    "url": "./static/media/logo.30d416d9.png"
+  },
+  {
+    "revision": "500f73baa0e774d835156542763a4953",
+    "url": "./static/media/logoIcon.500f73ba.png"
+  },
+  {
+    "revision": "3823365013fa0ad9df962d6342dea314",
+    "url": "./static/media/logoIconFace.38233650.png"
+  },
+  {
+    "revision": "9821534405ab102be5e55da4cd18e190",
+    "url": "./static/media/phone.98215344.png"
+  },
+  {
+    "revision": "d76791cd51199b76864aa7693f7b7ed6",
+    "url": "./static/media/pinezka.d76791cd.png"
+  },
+  {
+    "revision": "7e8746b387c4103e8c2f076becd837b5",
+    "url": "./static/media/zyczenia2019.7e8746b3.png"
+  }
+]);

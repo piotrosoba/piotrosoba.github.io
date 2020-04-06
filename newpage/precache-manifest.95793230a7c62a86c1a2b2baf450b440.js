9ff536334eb31ed7f0d5798a127ba048db2443fc
Piotr Osoba <piotrosoba@outlook.com>
2020-04-06 10:02:11 +0200

    new page

---
+++ newpage/precache-manifest.95793230a7c62a86c1a2b2baf450b440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2701a43700684acb668b71b8a2ad8293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f78daf578c26b04427",
+    "url": "/static/css/main.9fe1f63b.chunk.css"
+  },
+  {
+    "revision": "b2c93f5de8209880ee90",
+    "url": "/static/js/2.035021ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.035021ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f78daf578c26b04427",
+    "url": "/static/js/main.657461b5.chunk.js"
+  },
+  {
+    "revision": "78c12e4b92ca03390b59",
+    "url": "/static/js/runtime-main.92fb46c7.js"
+  }
+]);

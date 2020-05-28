8a37a3f55986bba7ec2bce282fb9def330456632
Piotr Osoba <piotrosoba@outlook.com>
2020-05-28 09:51:48 +0200

    it learning

---
+++ it-learning/precache-manifest.a07d282f8c39b502cc16e8a9505dbe20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cdb9e8576154457a7b8d3d4cd6f47b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b18cdfffba0a725f65b2",
+    "url": "./static/js/2.80ea080c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.80ea080c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c6456694cc224bb7efc",
+    "url": "./static/js/main.a3330923.chunk.js"
+  },
+  {
+    "revision": "ffc06f8e5df4f379daac",
+    "url": "./static/js/runtime-main.d101ad4c.js"
+  },
+  {
+    "revision": "408a75387e98d2c52a3dc0e8e0397b91",
+    "url": "./static/media/learnign.408a7538.jpg"
+  }
+]);
